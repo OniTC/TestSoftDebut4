7f828ae9c9017f9edb63568dc7fd8824237bb53e
sumeth_sr <sumeth.s@triplep.tech>
2023-09-04 22:07:41 +0700

    Finished Test

---
+++ TestSoftDebut4/TestSoftDebut4/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using TestSoftDebut4.Models;
+using TestSoftDebut4.Data;
+
+namespace TestSoftDebut4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public HomeController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<EmployeeModel> employeeList = _db.Employee.ToList();
+
+            return View(employeeList);
+        }
+
+    }
+}
